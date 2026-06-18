d3231921e0d270123ca53ca46f113e6d1bc58408
Yael shraga <yael216220@gmail.com>
2026-06-19 01:52:58 +0300

    Refactor SmartScale.h header guard and formatting

--- Main_Controller_Uno/SmartScale.h
+++ Main_Controller_Uno/SmartScale.h
@@ -2,27 +2,25 @@
 #define SMART_SCALE_H
 
 #include <Arduino.h>
-#include <SoftwareSerial.h> // שימוש בספרייה לניהול התקשורת מהמקמ"ש [cite: 216]
+#include <SoftwareSerial.h> 
 
 class SmartScale {
 private:
-    SoftwareSerial& scaleSerial; // רפרנס לחיבור הטורי של המקמ"ש
-    const int weightLimit = 20;  // משקל סף להוצאת שקית [cite: 333, 447]
+    SoftwareSerial& scaleSerial; 
+    const int weightLimit = 20;  
     int lastReadWeight = 0;
 
 public:
-    // בנאי המקבל את אובייקט ה-SoftwareSerial
     SmartScale(SoftwareSerial& serial) : scaleSerial(serial) {}
 
     void init() {
-        scaleSerial.begin(9600); // קצב תקשורת 9600bps כפי שמדדת בסקופ [cite: 211, 295]
+        scaleSerial.begin(9600); 
     }
 
-    // פונקציה שבודקת אם הגיע נתון חדש מה-Pro Micro
     void update() {
         if (scaleSerial.available()) {
-            delay(10); // השהיה קלה להבטחת הגעת כל הביטים 
-            lastReadWeight = scaleSerial.read(); // קריאת הנתון שנשלח [cite: 229]
+            delay(10); 
+            lastReadWeight = scaleSerial.read(); 
             
             Serial.print("Received Weight: ");
             Serial.println(lastReadWeight);
@@ -33,9 +31,8 @@ public:
         return lastReadWeight;
     }
 
-    // בדיקה האם המשקל חורג מהמותר [cite: 441, 447]
     bool isOverweight() {
-        update(); // עדכון לפני הבדיקה
+        update();
         return lastReadWeight >= weightLimit;
     }
 };
